1eb62052e2dd2cad3efe6288573a5aa72c5a975c
Asim <98515091+EverythingASIM@users.noreply.github.com>
2022-01-30 05:25:46 +0800

    init

---
+++ Assets/Phyllotaxis/Phyllotaxis.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// http://algorithmicbotany.org/papers/abop/abop-ch4.pdf
+/// </summary>
+/// 
+public class Phyllotaxis : MonoBehaviour
+{
+    public int FloretNum = 100;
+    public float AngleOfDivergence = 137.5f;
+    public int scale = 4;
+    public float dotsize = 2;
+
+    Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
+
+    int currentFloretCount = 0;
+    float angle;
+    float radius;
+    float posX;
+    float posY;
+
+    void Update()
+    {
+        angle = currentFloretCount * AngleOfDivergence;
+        radius = scale * Mathf.Sqrt(currentFloretCount);
+        posX = radius * Mathf.Cos(angle) + center.x;
+        posY = radius * Mathf.Sin(angle) + center.y;
+
+        if (currentFloretCount < FloretNum)
+        {
+            currentFloretCount++;
+        }
+    }
+
+    void OnGUI()
+    {
+        _2DExtension.SetBGColor(Color.black);
+
+        Color32 color = Color.HSVToRGB(((angle - radius) % 255)/255, 1, 1);
+        _2DExtension.DrawDot(new Vector2(posX, posY), new Vector2(dotsize, dotsize), color, Color.black);
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Phyllotaxis 
+Simple Phyllotaxis Planar Model In Action
+<details>
+  <summary>Screenshot</summary>
+  
+  ![Phyllotaxis](https://user-images.githubusercontent.com/98515091/151350051-d0d72261-4f80-42b5-b5e6-dcb764dead80.PNG "asd" )
+  
+</details>
+
+# Sources
+- http://algorithmicbotany.org/papers/abop/abop-ch4.pdf
+- https://iamkkm-cmd.shinyapps.io/BeautyOfNature/
